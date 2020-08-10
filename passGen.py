45a54e4eb85e1b44f01cf52d56bc5c5ba1417795
mkb79 <mkb79@hackitall.de>
2020-08-10 11:46:19 +0200

    Update passGen.py

--- passGen.py
+++ passGen.py
@@ -1,59 +1,52 @@
 import random
 import string
 
-working = True
-while working == True:
+
+def ask_for_yes_no(question: str) -> bool:
+    while True:
+        answer = input(question + " [yes/no]: ").lower()
+        if answer in ["yes", "no"]:
+            break
+        print("Please answer with yes or no!")
+
+    return True if answer == "yes" else False
 
 
-    lenght = int(input("How long do you want the password to be? [in numbers] "))
-    wantNums = input("Do you want numbers in the password? [yes/no] ")
-    wantUppers = input("Do you want uppercase letters? [yes/no] ")
+def get_random_string(length: int,
+                      include_num: bool=False,
+                      include_uppercase: bool=False,
+                      include_special_chars: bool=False) -> str:
+    chars = string.ascii_lowercase
+    if include_num:
+        chars += string.digits
+    if include_uppercase:
+        chars += string.ascii_uppercase
+    if include_special_chars:
+        chars += string.punctuation
+
+    return "".join(random.choices(chars, k=length))
 
 
+if __name__ == "__main__":
+    while True:
+        length = int(
+            input("How long do you want the password to be? [in numbers]: "))
 
-    def passGen(lenght):
+        include_num = ask_for_yes_no("Do you want numbers in the password?")
 
-        if wantNums.lower() == "yes" and wantUppers.lower() == "yes":
-            password = ""
-            chars = string.ascii_letters + "1234567890"
-            for i in range(lenght):
-                password = password + random.choice(chars)
-            print(password)
+        include_uppercase = ask_for_yes_no("Do you want uppercase letters?")
 
+        include_special_chars = ask_for_yes_no(
+            "Do you want special characters?")
 
-        elif wantNums.lower() == "no" and wantUppers.lower() == "yes":
-            password = ""
-            chars = string.ascii_letters 
-            for i in range(lenght):
-                password = password + random.choice(chars)
-            print(password)
+        random_string = get_random_string(
+            length=length,
+            include_num=include_num,
+            include_uppercase=include_uppercase,
+            include_special_chars=include_special_chars)
+        print(random_string)
 
-        elif wantNums.lower() == "yes" and wantUppers.lower() == "no":
-            password = ""
-            chars = string.ascii_letters + "1234567890"
-            for i in range(lenght):
-                password = password + random.choice(chars)
-            print(password.lower())
-        
-        elif wantNums.lower() == "no" and wantUppers.lower() == "no":
-            password = ""
-            chars = string.ascii_letters
-            for i in range(lenght):
-                password = password + random.choice(chars)
-            print(password.lower())
-
-        else:
-            print("Invalid input, please enter yes or no! ")
-
-        
-
-                
-
-    passGen(lenght)
-
-    restart = input("Do you want another password? [yes/no] ")
-    if restart.lower() == "yes":
-        working = True
-    else:
-        print("Goodbye!")
-        working = False
+        restart = ask_for_yes_no("Do you want another password?")
+        if not restart:
+            print("Goodbye!")
+            break
